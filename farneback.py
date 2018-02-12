8a0b09274f927436c82b3d30f8277b6c9efff2b9
abhay-venkatesh <abhay.venkatesh@gmail.com>
2018-02-12 16:23:26 -0600

    Pretty good optical flow

--- farneback.py
+++ farneback.py
@@ -1,6 +1,5 @@
 """
 References:
-[1] - https://docs.opencv.org/3.1.0/d7/d8b/tutorial_py_lucas_kanade.html
 """
 
 import cv2
@@ -8,29 +7,29 @@ import numpy as np
 
 frame1 = cv2.imread('./images/pic0.png')
 frame2 = cv2.imread('./images/pic1.png')
+old_gt = cv2.imread('./images/seg0.png')
+old_gt = cv2.cvtColor(old_gt,cv2.COLOR_BGR2GRAY)
 previous_frame = cv2.cvtColor(frame1,cv2.COLOR_BGR2GRAY)
 next_frame = cv2.cvtColor(frame2,cv2.COLOR_BGR2GRAY)
+flow = cv2.calcOpticalFlowFarneback(previous_frame, next_frame, 0.5, 3, 15, 3, 5, 1.2, 0)
 
-hsv = np.zeros_like(frame1)
-hsv[...,1] = 255
+height = flow.shape[0]
+width = flow.shape[1]
+R2 = np.dstack(np.meshgrid(np.arange(width), np.arange(height)))
+pixel_map = R2 + flow
 
-i = 0
-while i < 4:
-    next_frame = cv2.cvtColor(frame2,cv2.COLOR_BGR2GRAY)
-    flow = cv2.calcOpticalFlowFarneback(previous_frame, next_frame, 0.5, 3, 15, 3, 5, 1.2, 0)
-    mag, ang = cv2.cartToPolar(flow[...,0], flow[...,1])
-    hsv[...,0] = ang*180/np.pi/2
-    hsv[...,2] = cv2.normalize(mag,None,0,255,cv2.NORM_MINMAX)
-    rgb = cv2.cvtColor(hsv,cv2.COLOR_HSV2BGR)
+pixel_map_x = np.zeros((height, width))
+for i in range(height):
+    for j in range(width):
+        pixel_map_x[i][j] = pixel_map[i][j][0]
 
-    cv2.imshow('frame2',rgb)
-    k = cv2.waitKey(30) & 0xff
-    if k == 27:
-        break
-    elif k == ord('s'):
-        cv2.imwrite('opticalmyhsv.pgm',rgb)
+pixel_map_y = np.zeros((height, width))
+for i in range(height):
+    for j in range(width):
+        pixel_map_y[i][j] = pixel_map[i][j][1]
 
-    i += 1
+pixel_map_x_32 = pixel_map_x.astype('float32')
+pixel_map_y_32 = pixel_map_y.astype('float32')
 
-cap.release()
-cv2.destroyAllWindows()
+new_gt = cv2.remap(old_gt, pixel_map_x_32, pixel_map_y_32, cv2.INTER_NEAREST)
+cv2.imwrite('seg1approx.png', new_gt)
